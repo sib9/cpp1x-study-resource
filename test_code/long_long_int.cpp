defb2aa58a064567706003d3429fbaf1aa1f434e
JerryZhang <chinajiezhang@gmail.com>
2013-08-03 14:22:53 +0800

    1.

---
+++ test_code/long_long_int.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << sizeof(int) << std::endl;
+    std::cout << sizeof(long int) << std::endl;
+    std::cout << sizeof(long long int) << std::endl;
+    
+    return 0;
+}
